c1adb63fbd35ae7f8cd8840318d40d9f1346057d
luca <luca.tornatore@inaf.it>
2026-04-15 20:46:32 +0200

    update openmp materials

--- OpenMP/examples/parallel_regions/helloworld.c
+++ OpenMP/examples/parallel_regions/helloworld.c
@@ -1,16 +1,15 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
 
-int main(void)
+
+int main ( void )
 {
-  int sum = 0;
- #pragma omp parallel
+  #pragma omp parallel
   {
     int myid = omp_get_thread_num();
-    sum += myid;
+    printf("hello world from thread %d\n", myid);
   }
-  printf("sum is %d\n", sum);
+  
   return 0;
 }
